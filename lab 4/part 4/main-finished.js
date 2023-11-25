e71c9b9da86c28490ced033137e2ea203c99da04
Umang <umang.gajera@dcmail.ca>
2023-11-24 22:49:38 -0500

    part 4
    Here, I make change in main.js file

--- lab 4/part 4/main-finished.js
+++ lab 4/part 4/main-finished.js
@@ -1,33 +1,47 @@
 // set up canvas
 
+const para = document.querySelector('p');
+let count = 0;
+
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
 const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
-
+const height = canvas.height = window.innerHeight; 
 // function to generate random number
 
-function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+function random(min,max) {
+   const num = Math.floor(Math.random()*(max-min)) + min;
+   return num;
+ };
+ 
 
 // function to generate random RGB color value
 
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+class Shape {
 
-class Ball {
-
-   constructor(x, y, velX, velY, color, size) {
-      this.x = x;
-      this.y = y;
-      this.velX = velX;
-      this.velY = velY;
-      this.color = color;
-      this.size = size;
+   constructor(x, y, velX, velY) {
+     this.x = x;
+     this.y = y;
+     this.velX = velX;
+     this.velY = velY;
    }
+ 
+ }
+ 
+ class Ball extends Shape {
+ 
+   constructor(x, y, velX, velY, color, size) {
+     super(x, y, velX, velY);
+ 
+     this.color = color;
+     this.size = size;
+     this.exists = true;
+   }
+ 
 
    draw() {
       ctx.beginPath();
