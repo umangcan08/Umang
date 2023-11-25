8312e975d9694f2aa416382a50e42b37ba2d20c9
Umang <umang.gajera@dcmail.ca>
2023-11-24 21:56:01 -0500

    part 2
    Here, I added main.js file and add comments

--- lab 4/part 2/main.js
+++ lab 4/part 2/main.js
@@ -1,18 +1,70 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+/* Creating an array of image file names */
+const imageArray = ['images/pic1.jpg', 'images/pic2.jpg', 'images/pic3.jpg', 'images/pic4.jpg', 'images/pic5.jpg'];
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+/* Looping through images and creating thumbnail images */
+function loopingImg() {
+  for (let i = 1; i <= imageArray.length; i++) {
+    // Create a new image element for each image in the array
+    let newImage = document.createElement('img');
+    
+    // Get the file path for the current image
+    let image = imageArray[i - 1]; // Adjusted index to start from 1
+    
+    // Set the 'src' attribute of the new image element
+    newImage.setAttribute('src', image);
+    
+    // Append the new image element to the '.thumb-bar' container in the HTML
+    document.querySelector('.thumb-bar').appendChild(newImage);
+  }
+}
 
-/* Declaring the array of image filenames */
+/* Adding an event listener for the "click" event on all thumbnails */
+function clickImg() {
+  // Function to set the 'src' attribute of the displayed image when a thumbnail is clicked
+  function setImgSrc() {
+    // 'this' points to the clicked element (thumbnail)
+    let attribute = this.getAttribute('src');
+    let displayedImage = document.querySelector('.displayed-img');
+    
+    // Set the 'src' attribute of the displayed image to the clicked thumbnail's 'src'
+    displayedImage.setAttribute('src', attribute);
+  }
 
-/* Declaring the alternative text for each image file */
+  // Select all thumbnail images and add a 'click' event listener to each
+  let elements = document.querySelectorAll('.thumb-bar img');
+  elements.forEach(function (element) {
+    element.addEventListener('click', setImgSrc);
+  });
+}
 
-/* Looping through images */
+/* Handler to lighten/darken the displayed image with a button click */
+function darkenImg() {
+  // Get references to the button and overlay elements
+  let buttonNode = document.querySelector('.dark');
+  let overlay = document.querySelector('.overlay');
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+  // Add a 'click' event listener to the button
+  buttonNode.addEventListener('click', () => {
+    // Get the current class of the button
+    const btnClass = buttonNode.getAttribute('class');
 
-/* Wiring up the Darken/Lighten button */
+    // Toggle between lightening and darkening the image and update button text
+    if (btnClass === 'dark') {
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+      buttonNode.setAttribute('class', 'light');
+      buttonNode.textContent = 'Lighten';
+    } else {
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+      buttonNode.setAttribute('class', 'dark');
+      buttonNode.textContent = 'Darken';
+    }
+  });
+}
+
+// Using the 'DOMContentLoaded' event to ensure the DOM is fully loaded before executing the code
+document.addEventListener('DOMContentLoaded', (event) => {
+  // Call the functions to set up the image gallery
+  loopingImg();
+  clickImg();
+  darkenImg();
+});
