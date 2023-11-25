54564c6b4ef1f237c006395304cc202260fbcf94
Umang <umang.gajera@dcmail.ca>
2023-11-24 22:24:51 -0500

    part 3
    Here, I added comments in main.js file

--- lab 4/part 3/main.js
+++ lab 4/part 3/main.js
@@ -1,20 +1,99 @@
-// setup canvas
-
+// Canvas setup
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-const width = (canvas.width = window.innerWidth);
-const height = (canvas.height = window.innerHeight);
+// Set canvas dimensions to match the window size
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
 
-// function to generate random number
-
+// Random number generation functions
 function random(min, max) {
-  const num = Math.floor(Math.random() * (max - min + 1)) + min;
-  return num;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-// function to generate random color
 
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+
+// Definition of the Ball class
+class Ball {
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+
+  // Method to draw the ball on the canvas
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  // Method to update the ball's position
+  update() {
+    if ((this.x + this.size) >= width || (this.x - this.size) <= 0) {
+      this.velX = -Math.abs(this.velX);
+    }
+
+    if ((this.y + this.size) >= height || (this.y - this.size) <= 0) {
+      this.velY = -Math.abs(this.velY);
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+
+  // Method to detect collisions between balls
+  collisionDetect() {
+    for (const ball of balls) {
+      if (!(this === ball)) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+}
+
+// Initialize an array of balls with random properties
+const balls = [];
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size
+  );
+
+  balls.push(ball);
+}
+
+// Animation loop to continuously update and draw balls on the canvas
+function loop() {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+// Start the animation loop
+loop();
